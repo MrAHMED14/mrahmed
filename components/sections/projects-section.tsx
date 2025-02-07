bad983786622d126dc3cf263bfa0d33ec472eaaf
MrAHMED <chikhaoui084@gmail.com>
2025-02-07 15:58:46 -0800

    feat: implement ProjectCard component for dynamic project display and add new project

--- components/sections/projects-section.tsx
+++ components/sections/projects-section.tsx
@@ -1,13 +1,62 @@
 import { RiReactjsFill, RiNextjsLine, RiTailwindCssFill } from "react-icons/ri"
 import { SiPostgresql } from "react-icons/si"
-import { Badge } from "@/components/ui/badge"
+
 import ProjectSkeleton from "@/components/global/project-skeleton"
-import Image from "next/image"
-import Link from "next/link"
+import ProjectCard from "../global/project-card"
 
-interface ProjectsSectionProps {}
+const projects = [
+  {
+    title: "Cortex store",
+    desc: "E-commerce website",
+    href: "https://cortex-store.vercel.app",
+    image: "/cortex-cover.png",
+    badge: [
+      {
+        name: "ReactJS",
+        icon: <RiReactjsFill className="mr-1 h-4 w-4 flex-shrink-0" />,
+      },
+      {
+        name: "NextJS",
+        icon: <RiNextjsLine className="mr-1 h-4 w-4 flex-shrink-0" />,
+      },
+      {
+        name: "Tailwind css",
+        icon: <RiTailwindCssFill className="mr-1 h-4 w-4 flex-shrink-0" />,
+      },
+      {
+        name: "PostgreSQL",
+        icon: <SiPostgresql className="mr-1 h-4 w-4 flex-shrink-0" />,
+      },
+    ],
+  },
+  {
+    title: "University calculator",
+    desc: "Tool website",
+    href: "https://university-calculator.vercel.app",
+    image: "/univ-calculator-cover.png",
+    badge: [
+      {
+        name: "NextJS",
+        icon: <RiNextjsLine className="mr-1 h-4 w-4 flex-shrink-0" />,
+      },
+      {
+        name: "ReactJS",
+        icon: <RiReactjsFill className="mr-1 h-4 w-4 flex-shrink-0" />,
+      },
+      {
+        name: "PostgreSQL",
+        icon: <SiPostgresql className="mr-1 h-4 w-4 flex-shrink-0" />,
+      },
 
-export default function ProjectsSection({}: ProjectsSectionProps) {
+      {
+        name: "Tailwind css",
+        icon: <RiTailwindCssFill className="mr-1 h-4 w-4 flex-shrink-0" />,
+      },
+    ],
+  },
+]
+
+export default function ProjectsSection() {
   return (
     <div className="w-[90%] sm:w-[70%] sm:mx-12 mx-4 mb-72">
       <h1
@@ -17,54 +66,16 @@ export default function ProjectsSection({}: ProjectsSectionProps) {
         MY Projects
       </h1>
       <div className="flex flex-col gap-6">
-        <Link
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://cortex-store.vercel.app"
-        >
-          <div className="max-sm:flex-col sm:flex gap-6 max-sm:justify-center max-sm:items-center sm:items-center p-5 bg-white/10  sm:h-40 rounded-sm">
-            <div className="max-sm:w-full max-sm:mb-2  max-sm:flex max-sm:items-center max-sm:justify-center">
-              <div className="w-full max-h-80 sm:w-48 sm:h-32 rounded bg-white/20">
-                <Image
-                  src={"/cortex-cover.png"}
-                  width={192}
-                  height={192}
-                  alt="project 01"
-                  className="relative w-full h-full sm:w-48 sm:h-32 object-cover object-center rounded-sm"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-3">
-              <div className="">
-                <h1 className="text-xl sm:text-2xl font-bold">Cortex store</h1>
-                <p>E-commerce website</p>
-                <Badge variant="destructive" className="text-white">
-                  <span className="font-bold pr-1">Status: </span>Not completed
-                </Badge>
-              </div>
-              <div className="flex flex-wrap gap-x-4 gap-y-2">
-                <Badge variant="outline" className="text-xs border-gray-50/30">
-                  <RiReactjsFill className="mr-1 h-4 w-4 flex-shrink-0" />
-                  ReactJS
-                </Badge>
-                <Badge variant="outline" className="text-xs border-gray-50/30">
-                  <RiNextjsLine className="mr-1 h-4 w-4 flex-shrink-0" />
-                  NextJS
-                </Badge>
-                <Badge variant="outline" className="text-xs border-gray-50/30">
-                  <RiTailwindCssFill className="mr-1 h-4 w-4 flex-shrink-0" />
-                  Tailwind css
-                </Badge>
-                <Badge variant="outline" className="text-xs border-gray-50/30">
-                  <SiPostgresql className="mr-1 h-4 w-4 flex-shrink-0" />
-                  PostgreSQL
-                </Badge>
-              </div>
-            </div>
-          </div>
-        </Link>
-
-        <ProjectSkeleton />
+        {projects.map((project, index) => (
+          <ProjectCard
+            key={index}
+            title={project.title}
+            desc={project.desc}
+            image={project.image}
+            href={project.href}
+            badges={project.badge}
+          />
+        ))}
         <ProjectSkeleton />
         <ProjectSkeleton />
       </div>
